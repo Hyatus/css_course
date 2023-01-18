a1dbf7e35d9b644ebafc8415ceff1a666192b8f7
Hyatus <mejia.cristian583@gmail.com>
2023-01-17 23:48:33 -0600

    CAFETERIA PT2

--- Cafeteria/gulpfile.js
+++ Cafeteria/gulpfile.js
@@ -3,9 +3,40 @@ const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
+
+
+//IMAGENES 
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function imagenes(done){
+    src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(dest('build/img'))
+    done();    
+}
+
+function versionWebp(){
+    // De todas la imagenes que encuentre con extensión .png y jpg
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp())
+        .pipe(dest('build/img'))
+}
+
+function versionAvif(){
+    const opciones = {
+        quality:50
+    }
+    // De todas la imagenes que encuentre con extensión .png y jpg
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+}
+
+
 function css(done){
     // Compilar SASS
-
     // PASOS:
     // 1 -Identificar archivo
     // 2 - Compilar
@@ -21,16 +52,17 @@ function css(done){
 function dev(){
     // toma 2 valores: qué reviso  y función a ejecutar
     watch('src/scss/**/*.scss',css)
+    watch('src/img/**/*',imagenes)
     //watch('src/scss/app.scss',css);
 }
 
-function tareadefault(){
-    console.log('Soy la tarea default')
-}
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css,dev);
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes,versionWebp,versionAvif, css,dev);
 
 
 //series - Se inicia una tarea y hasta que finaliza inicia la siguiente
